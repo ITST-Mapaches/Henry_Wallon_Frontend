6c4a2636537864ba2b2efc19a82aa142727c3584
Humberto Domínguez <humbertodelac13@gmail.com>
2024-04-30 23:29:20 -0600

    librerias para UI agregadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {
       colors: {
-        'primary': '#21182F',
-        'secondary': '#F1E62A'
+        primary: "#21182F",
+        secondary: "#F1E62A",
+        current: "currentColor",
+      },
+      animation: {
+        "pulse-fast": "pulse 1s linear infinite",
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };

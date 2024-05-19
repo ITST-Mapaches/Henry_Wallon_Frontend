a42c87e03461a8c1a90d043a930c0dc6f47cd9f0
Humberto Domínguez <humbertodelac13@gmail.com>
2024-05-19 13:28:51 -0600

    page administrador: home - clases eliminadas

--- src/components/Administrator/Home.jsx
+++ src/components/Administrator/Home.jsx
@@ -15,50 +15,50 @@ const Home = () => {
             ||
             (<div className="grid grid-cols-flex-grid gap-8">
                 <div className="div shadow-lg shadow-gray-300 h-[8em] w-[15em] bg-slate-200 m-auto rounded-[1em] overflow-hidden relative group p-2 z-0 flex items-center justify-center cursor-pointer hover:scale-105">
-                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-rose-500 group-hover:scale-[800%] duration-500 z-[-1] op"></div>
-                  <h1 className="z-20 font-bold font-Poppin group-hover:text-white duration-500 text-[1.4em] text-center">
+                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-rose-500 group-hover:scale-[800%] duration-200 z-[-1]"></div>
+                  <h1 className="z-20 font-bold group-hover:text-white duration-400 text-[1.4em] text-center">
                     <span className="text-4xl font-black">{data.numero_mujeres}</span> <br/> Mujeres
                   </h1>
                 </div>
                 <div className="div shadow-lg shadow-gray-300 h-[8em] w-[15em] bg-slate-200 m-auto rounded-[1em] overflow-hidden relative group p-2 z-0 flex items-center justify-center cursor-pointer hover:scale-105">
-                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-sky-500 group-hover:scale-[800%] duration-500 z-[-1] op"></div>
-                  <h1 className="z-20 font-bold font-Poppin group-hover:text-white duration-500 text-[1.4em] text-center">
+                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-sky-500 group-hover:scale-[800%] duration-200 z-[-1]"></div>
+                  <h1 className="z-20 font-bold group-hover:text-white duration-400 text-[1.4em] text-center">
                     <span className="text-4xl font-black">{data.numero_hombres}</span> <br/> Hombres
                   </h1>
                 </div>
                 <div className="div shadow-lg shadow-gray-300 h-[8em] w-[15em] bg-slate-200 m-auto rounded-[1em] overflow-hidden relative group p-2 z-0 flex items-center justify-center cursor-pointer hover:scale-105">
-                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-lime-500 group-hover:scale-[800%] duration-500 z-[-1] op"></div>
-                  <h1 className="z-20 font-bold font-Poppin group-hover:text-white duration-500 text-[1.4em] text-center">
+                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-lime-500 group-hover:scale-[800%] duration-200 z-[-1]"></div>
+                  <h1 className="z-20 font-bold group-hover:text-white duration-400 text-[1.4em] text-center">
                     <span className="text-4xl font-black">{data.numero_cuentas_activas}</span> <br/> Cuentas activas
                   </h1>
                 </div>
                 <div className="div shadow-lg shadow-gray-300 h-[8em] w-[15em] bg-slate-200 m-auto rounded-[1em] overflow-hidden relative group p-2 z-0 flex items-center justify-center cursor-pointer hover:scale-105">
-                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-red-600 group-hover:scale-[800%] duration-500 z-[-1] op"></div>
-                  <h1 className="z-20 font-bold font-Poppin group-hover:text-white duration-500 text-[1.4em] text-center">
+                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-red-600 group-hover:scale-[800%] duration-200 z-[-1]"></div>
+                  <h1 className="z-20 font-bold group-hover:text-white duration-400 text-[1.4em] text-center">
                     <span className="text-4xl font-black">{data.numero_cuentas_inactivas}</span> <br/> Cuentas inactivas
                   </h1>
                 </div>
                 <div className="div shadow-lg shadow-gray-300 h-[8em] w-[15em] bg-slate-200 m-auto rounded-[1em] overflow-hidden relative group p-2 z-0 flex items-center justify-center cursor-pointer hover:scale-105">
-                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-pink-500 group-hover:scale-[800%] duration-500 z-[-1] op"></div>
-                  <h1 className="z-20 font-bold font-Poppin group-hover:text-white duration-500 text-[1.4em] text-center">
+                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-pink-500 group-hover:scale-[800%] duration-200 z-[-1]"></div>
+                  <h1 className="z-20 font-bold group-hover:text-white duration-400 text-[1.4em] text-center">
                     <span className="text-4xl font-black">{data.numero_administradores}</span> <br/> Administrador (es)
                   </h1>
                 </div>
                 <div className="div shadow-lg shadow-gray-300 h-[8em] w-[15em] bg-slate-200 m-auto rounded-[1em] overflow-hidden relative group p-2 z-0 flex items-center justify-center cursor-pointer hover:scale-105">
-                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-green-500 group-hover:scale-[800%] duration-500 z-[-1] op"></div>
-                  <h1 className="z-20 font-bold font-Poppin group-hover:text-white duration-500 text-[1.4em] text-center">
+                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-green-500 group-hover:scale-[800%] duration-200 z-[-1]"></div>
+                  <h1 className="z-20 font-bold group-hover:text-white duration-400 text-[1.4em] text-center">
                     <span className="text-4xl font-black">{data.numero_alumnos}</span> <br/> Alumno (s)
                   </h1>
                 </div>
                 <div className="div shadow-lg shadow-gray-300 h-[8em] w-[15em] bg-slate-200 m-auto rounded-[1em] overflow-hidden relative group p-2 z-0 flex items-center justify-center cursor-pointer hover:scale-105">
-                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-indigo-500 group-hover:scale-[800%] duration-500 z-[-1] op"></div>
-                  <h1 className="z-20 font-bold font-Poppin group-hover:text-white duration-500 text-[1.4em] text-center">
+                  <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-indigo-500 group-hover:scale-[800%] duration-200 z-[-1]"></div>
+                  <h1 className="z-20 font-bold group-hover:text-white duration-400 text-[1.4em] text-center">
                     <span className="text-4xl font-black">{data.numero_docentes}</span> <br/> Docente (s)
                   </h1>
                 </div>
                 <div className="div shadow-lg shadow-gray-300 h-[8em] w-[15em] bg-slate-200 m-auto rounded-[1em] overflow-hidden relative group p-2 z-0 flex items-center justify-center cursor-pointer hover:scale-105">
-                    <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-red-500 group-hover:scale-[800%] duration-500 z-[-1] op"></div>
-                    <h1 className="z-20 font-bold font-Poppin group-hover:text-white duration-500 text-[1.4em] text-center">
+                    <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-red-500 group-hover:scale-[800%] duration-200 z-[-1]"></div>
+                    <h1 className="z-20 font-bold group-hover:text-white duration-400 text-[1.4em] text-center">
                       <span className="text-4xl font-black">{data.numero_tutores}</span> <br/> Tutor (es)
                     </h1>
                 </div>
